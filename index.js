892b5e40229077c5cc4c856ffcb6fa3e88372c0c
Lucas <lucas.vianam@outlook.com>
2022-04-11 22:57:23 -0300

    .

--- index.js
+++ index.js
@@ -2,10 +2,43 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/app', (req, res) => {
-    res.send('Hello Blue Módulo 3 Fullstack');
-})
+// Configuração para usar JSON'a
+app.use(express.json());
+
+// Definir dados para usar na aplicação
+const paletas = [
+  {
+    id: 1,
+    sabor: 'Açai com Leite Condensado',
+    descricao: 'Açai com Leite Condensado',
+    foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
+    preco: 10.0,
+  },
+  {
+    id: 2,
+    sabor: 'Banana com Nutella',
+    descricao: 'Banana com Nutella',
+    foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
+    preco: 10.0,
+  },
+  {
+    id: 3,
+    sabor: 'Chocolate Belga',
+    descricao: 'Chocolate Belga',
+    foto: 'https://storage.googleapis.com/domain-images/60b1f285-d77c-444a-b734-1bc1efd2c472/products/gallery_bd17d9a3-cd93-44a9-9b44-f017a1c7a329.jpg',
+    preco: 7.0,
+  },
+];
+
+app.get('/', (req, res) => {
+  res.send('Hello Blue Módulo 3 Fullstack');
+});
+
+app.get('/paletas/find-paletas', (req, res) => {
+    res.send(paletas)
+  });
+  
 
 app.listen(port, () => {
-    console.log(`Rodando em http://localhost:${port} ❤️`)
-})
+  console.log(`Rodando em http://localhost:${port}`);
+});
