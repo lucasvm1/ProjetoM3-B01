b8a8bf87df6fa09690a194844ea23be13d79ca3d
Lucas Viana <lucas.vianam@outlook.com>
2022-04-22 19:30:25 -0300

    MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const connectToDatabase = require('./src/database/database');
+
 const port = 3000;
 const app = express();
 const route = require('./src/routes/paletas.route');
 
+connectToDatabase();
 app.use(cors());
 app.use(express.json());
-
 app.use('/paletas', route);
 
 app.listen(port, () => {
--- src/controllers/paletas.controller.js
+++ src/controllers/paletas.controller.js
@@ -1,16 +1,25 @@
+const mongoose = require('mongoose');
 const paletasService = require('../services/paletas.service');
 
-const findPaletasController = (req, res) => {
-  const allPaletas = paletasService.findPaletasService();
+const findPaletasController = async (req, res) => {
+  const allPaletas = await paletasService.findPaletasService();
   res.send(allPaletas);
 };
 
-const findPaletaByIdController = (req, res) => {
+const findPaletaByIdController = async (req, res) => {
   const idParam = req.params.id;
-  if (!idParam) {
-    return res.status(404).send({ message: "Paleta não encontrada!" })
+
+  if (!mongoose.Types.ObjectId.isValid(idParam)) {
+    res.status(400).send({ message: 'ID inválido!' });
+    return;
   }
-  const chosenPaleta = paletasService.findPaletaByIdService(idParam);
+
+  const chosenPaleta = await paletasService.findPaletaByIdService(idParam);
+
+  if (!chosenPaleta) {
+    return res.status(404).send({ message: 'Paleta não encontrada!' });
+  }
+
   res.send(chosenPaleta);
 };
 
@@ -23,7 +32,12 @@ const createPaletaController = (req, res) => {
     !paleta.foto ||
     !paleta.preco
   ) {
-    return res.status(400).send({ mensagem: "Você não preencheu todos os dados para adicionar uma nova paleta ao cardápio!" });
+    return res
+      .status(400)
+      .send({
+        mensagem:
+          'Você não preencheu todos os dados para adicionar uma nova paleta ao cardápio!',
+      });
   }
   const newPaleta = paletasService.createPaletaService(paleta);
   res.send(newPaleta);
@@ -33,11 +47,21 @@ const updatePaletaController = (req, res) => {
   const idParam = Number(req.params.id);
   const paletaEdit = req.body;
   if (!idParam) {
-    return res.status(404).send({ message: "Paleta não encontrada!" })
+    return res.status(404).send({ message: 'Paleta não encontrada!' });
   }
 
-  if (!paletaEdit || !paletaEdit.sabor || !paletaEdit.descricao || !paletaEdit.foto || !paletaEdit.preco) {
-    return res.status(400).send({ message: "Você não preencheu todos os dados para editar a paleta!" });
+  if (
+    !paletaEdit ||
+    !paletaEdit.sabor ||
+    !paletaEdit.descricao ||
+    !paletaEdit.foto ||
+    !paletaEdit.preco
+  ) {
+    return res
+      .status(400)
+      .send({
+        message: 'Você não preencheu todos os dados para editar a paleta!',
+      });
   }
   const updatedPaleta = paletasService.updatePaletaService(idParam, paletaEdit);
   res.send(updatedPaleta);
@@ -46,17 +70,16 @@ const updatePaletaController = (req, res) => {
 const deletePaletaController = (req, res) => {
   const idParam = req.params.id;
   if (!idParam) {
-    return res.status(404).send({ message: "Paleta não encontrada!" })
+    return res.status(404).send({ message: 'Paleta não encontrada!' });
   }
   paletasService.deletePaletaService(idParam);
   res.send({ message: 'Paleta deletada com sucesso!' });
 };
-
 
 module.exports = {
   findPaletasController,
   findPaletaByIdController,
   createPaletaController,
   updatePaletaController,
-  deletePaletaController
+  deletePaletaController,
 };
