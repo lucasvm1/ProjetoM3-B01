45a101bd6ee32f3987f8d99b768a35c298ad1a23
Lucas Viana <lucas.vianam@outlook.com>
2022-04-25 19:16:38 -0300

    Correção de Bugs e Adição do carrinho

--- src/middlewares/paleta.middleware.js
+++ src/middlewares/paleta.middleware.js
@@ -24,7 +24,20 @@ const validObjectBody = (req, res, next) => {
   next();
 };
 
+const validObjectBodyCarrinho = (req, res, next) => {
+  const carrinho = req.body;
+
+  carrinho.forEach((item) => {
+    if (!item || !item.paletaId || !item.quantidade) {
+      return res
+        .status(400)
+        .send({ message: 'Envie o todos os campos das paletas!' });
+    }
+  });
+};
+
 module.exports = {
   validId,
   validObjectBody,
+  validObjectBodyCarrinho
 };
--- src/routes/paletas.route.js
+++ src/routes/paletas.route.js
@@ -1,13 +1,47 @@
 const express = require('express');
 const router = express.Router();
-const { validId, validObjectBody } = require('../middlewares/paleta.middleware');
-
+const {
+  validId,
+  validObjectBody,
+  validObjectBodyCarrinho,
+} = require('../middlewares/paleta.middleware');
 const paletasController = require('../controllers/paletas.controller');
 
+
 router.get('/all-paletas', paletasController.findPaletasController);
-router.get('/one-paleta/:id', validId, paletasController.findPaletaByIdController);
-router.post('/create-paleta', validId, paletasController.createPaletaController);
-router.put('/update-paleta/:id', validId, validObjectBody, paletasController.updatePaletaController);
-router.delete('/delete-paleta/:id', validId, paletasController.deletePaletaController);
+router.get(
+  '/one-paleta/:id',
+  validId,
+  paletasController.findPaletaByIdController,
+);
+router.post(
+  '/create-paleta',
+  validId,
+  paletasController.createPaletaController,
+);
+router.put(
+  '/update-paleta/:id',
+  validId,
+  validObjectBody,
+  paletasController.updatePaletaController,
+);
+router.delete(
+  '/delete-paleta/:id',
+  validId,
+  paletasController.deletePaletaController,
+);
+
+const controllerCarinho = require('../controllers/carrinho.controller');
+
+router.get('/all-carrinho', controllerCarinho.findAllCarrinhoController);
+router.post(
+  '/create-carrinho',
+  validObjectBodyCarrinho,
+  controllerCarinho.createManyItemsCarrinhoController,
+);
+router.delete(
+  '/finish-carrinho',
+  controllerCarinho.deleteAllItemsCarrinhoController,
+);
 
 module.exports = router;
